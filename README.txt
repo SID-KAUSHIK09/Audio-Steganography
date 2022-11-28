7e8bdd1ab743ccfc19d3ef38613088ca9bcb59dd
SIDDHARTH KAUSHIK <108971849+SID-KAUSHIK09@users.noreply.github.com>
2022-11-28 16:31:30 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+• Information Security Project, using Python Libraries
+• Encryption- Text is hidden in an image which is made as cover of an audio
+• Decryption- Text is retrieved from audio file using a temporary image
---
+++ encode.py
@@ -0,0 +1,21 @@
+from stepic import encode
+from eyed3 import load
+from PIL import Image
+
+print("####################")
+data=str(input("YOUR DATA: "))
+
+audio=input("Audio: ")
+img_name=input("IMAGE: ")
+audio=load(audio)
+
+img=Image.open(img_name)
+img_stegano=encode(img,data.encode())
+img_stegano.save(img_name)
+
+audio.initTag()
+audio.tag.images.set(3, open(img_name,"rb").read(),"image/png")
+audio.tag.save()
+
+print("####################")
+input("COMPLETE(press enter--> exit)")
